a9617cc99d99a2ede23ca6e54a580318d952fac6
Unknown <mark.collier11@hotmail.com>
2018-03-18 17:16:15 +0000

    what we believe

--- resources/views/we_believe.blade.php
+++ resources/views/we_believe.blade.php
@@ -5,58 +5,214 @@
     <div class='parallax' style="background-image: url('../images/Page-Images/activities/bible.jpeg');"></div>
 
     <!--<div class='parallax' style="background-image: url('../images/parallax/1.jpg');"></div>-->
-    <div class='page-Content'>
+    <div class="row-modual col-md-12">
 
-        <h2>What We Believe</h2>
+        <h1>What We Believe</h1>
 
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non velit augue. Nam suscipit convallis orci,
-            vitae faucibus lacus porttitor vel. Etiam fermentum erat mauris, id molestie massa gravida
-            vitae. Quisque cursus massa non nisl rhoncus venenatis. Curabitur et porttitor nisi, et porta tortor.
-            Praesent dignissim ante sit amet dui accumsan, nec
-            vulputate lectus vestibulum. Vivamus sagittis mattis sem sed suscipit. Nullam pharetra pulvinar volutpat.Ut
-            condimentum facilisis tempor. Nunc tincidunt
-            mi quis pretium lacinia. Nullam eget augue consequat, elementum felis ac, hendrerit odio. Nulla in neque ut
-            augue fermentum
-            sagittis. Aenean id posuere tortor, et consequat eros. Pellentesque
+        <div id="we-believe-content" >
+            <ul>
 
-        </p>
 
-        <p>
-            felis diam, pellentesque sit amet porta in, pellentesque non tellus. Cras sed sapien magna. Proin feugiat
-            pellentesque nisl. Suspendisse metus erat, fermentum nec iaculis sed, laoreet at mi. Nunc et tellus quam.
-            Sed et tortor ipsum. Integer ut lorem non dolor pretium tincidunt.
+                <li class="wwb-li">
+                    <p class="wwb-title">1. We believe that the Bible (i.e. the Old and New Testaments excluding the
+                        Apocrypha)</p>
 
-        </p>
+                    <p class="wwb-title-sup"> is the inspired Word of God, the infallible, all sufficient rule for faith
+                        and practice.</p>
 
-        <p>
-            Cras vel lacinia lectus. Integer ante metus, varius eu dui id, efficitur ultrices erat. Orci varius natoque
-            penatibus
-            et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per
-            conubia
-            nostra, per inceptos himenaeos. Aliquam id efficitur mi. Vivamus sodales semper lobortis. Ut sed lacus erat.
-            Nunc
-            ornare elit id arcu accumsan gravida.
+                    <ul class="scriptures">
+                        <li>2 Tim. 3:15-16</li>
+                        <li>2 Peter 1:21</li>
+                    </ul>
+                </li>
 
-        </p>
 
-        <p>
-            Sed porttitor mi vel ultricies volutpat. Quisque vehicula, sapien eu dignissim fermentum, ligula risus
-            lobortis enim, eu
-            rutrum sem leo ut urna. Maecenas euismod, risus scelerisque gravida lobortis, risus risus bibendum est, a
-            malesuada ante
-            sapien ac elit. Curabitur vehicula tincidunt quam ac porttitor. Maecenas nec ultricies massa. Vivamus dictum
-            lorem ut
-            aliquet laoreet. Ut placerat eget mi nec aliquam. Donec et justo eget velit elementum congue.
-        </p>
+                <li class="wwb-li">
+                    <p class="wwb-title">2. We believe in the unity of the One True and Living God who is the Eternal,
+                        Self-Existent "I AM"</p>
 
-        <p>
-            Donec enim arcu, posuere ac efficitur elementum, tincidunt eget nunc. Vivamus venenatis rhoncus venenatis.
-            Quisque ornare ex sit amet tempor euismod. Donec rutrum tempus nibh, et rhoncus nisi tincidunt elementum.
-            Maecenas nulla ligula, imperdiet eu posuere ac, faucibus id enim. Curabitur mi nisi, finibus a euismod ut,
-            convallis a quam. Quisque euismod urna augue, vitae dignissim tellus laoreet id. Pellentesque scelerisque
-            nunc sed nibh viverra, ac accumsan purus gravida.
-        </p>
+                    <p class="wwb-title-sup"> Who has also revealed Himself as One being co-existing in three Persons -
+                        Father, Son and Holy Spirit</p>
+
+                    <ul class="scriptures">
+                        <li>Deuteronomy 6:4</li>
+                        <li>Mark 12:29</li>
+                        <li>Matt 28:19</li>
+                        <li>2 Corinthians 13:14</li>
+                    </ul>
+                </li>
+
+
+                <li class="wwb-li">
+                    <p class="wwb-title">3. We believe in the Virgin Birth, Sinless Life, Miraculous Ministry,
+                        Substitutionary Atoning Death, Bodily Resurrection</p>
+
+                    <p class="wwb-title-sup">Triumphant Ascension and Abiding Intercession of the Lord Jesus Christ
+                        and in His personal, visible, bodily return in power and glory as the blessed hope of all
+                        believers </p>
+
+                    <ul class="scriptures">
+                        <li>Isaiah 7:14</li>
+                        <li>Acts 2:22, 10:38</li>
+                        <li>Luke 24:39</li>
+                        <li>Ephesians 4:8-10</li>
+                        <li>1 Corinthians 15:22-24, 51-57</li>
+                        <li>Matthew 1:23</li>
+                        <li>2 Corinthians 5:21</li>
+                        <li>1 Corinthians 15:4</li>
+                        <li>Romans 8:34</li>
+                        <li>1 Thessalonians 4:13-18</li>
+                        <li>Hebrews 7:26</li>
+                        <li>Hebrews 9:12</li>
+                        <li>Acts 1:9</li>
+                        <li>Hebrews 7:25</li>
+                        <li>Revelation 20:1-6</li>
+                        <li>1 Peter 2:22</li>
+                    </ul>
+
+                </li>
+
+
+                <li class="wwb-li">
+                    <p class="wwb-title">4. We believe in the fall of man</p>
+
+                    <p class="wwb-title-sup"> who was created pure and upright, but fell by voluntary transgression.</p>
+
+                    <ul class="scriptures">
+                        <li>Genesis 1:26-31, 3:1-7</li>
+                        <li>Romans 5:12-21</li>
+                    </ul>
+
+                </li>
+
+
+                <li class="wwb-li">
+                    <p class="wwb-title">5. We believe in salvation through faith in Christ</p>
+
+                    <p class="wwb-title-sup"> who, according to the Scriptures, died for our sins, was buried and was
+                        raised from the dead on the third day, and that through His Blood we have Redemption.</p>
+
+                    <ul class="scriptures">
+                        <li>Titus 2:11, 3:5-7</li>
+                        <li>Romans 10:8-15</li>
+                        <li>1 Corinthians 15:3-4</li>
+                    </ul>
+
+                </li>
+
+
+                <li class="wwb-li">
+                    <p class="wwb-title">6. This experience is also known as the new birth</p>
+
+                    <p class="wwb-title-sup"> and is an instantaneous and complete operation of the Holy Spirit upon
+                        initial faith in the Lord Jesus Christ.</p>
+
+                    <ul class="scriptures">
+                        <li>John 3:5-6</li>
+                        <li>James 1:18</li>
+                        <li>1 Peter 1:23</li>
+                        <li>1 John 5:1</li>
+                    </ul>
+                </li>
+
+
+                <li class="wwb-li">
+                    <p class="wwb-title">7. We believe that all who have truly repented and believed in Christ as Lord
+                        and Saviour are</p>
+
+                    <p class="wwb-title-sup"> commanded to be baptised by immersion in water.</p>
+
+                    <ul class="scriptures">
+                        <li>Matt. 28:19</li>
+                        <li>Acts 10:47-48</li>
+                        <li>Acts 2:38-39</li>
+                    </ul>
+                </li>
+
+
+                <li class="wwb-li">
+                    <p class="wwb-title">8. We believe in the baptism in the Holy Spirit</p>
+
+                    <p class="wwb-title-sup"> as an enduement of the believer with power for service, the essential,
+                        biblical evidence of which is the speaking with other tongues as the Spirit gives utterance.</p>
+
+                    <ul class="scriptures">
+                        <li>Acts 1:4-5, 8, 2:4, 10:44-46, 11:14-16, 19:6</li>
+                    </ul>
+                </li>
+
+
+                <li class="wwb-li">
+                    <p class="wwb-title">9. We believe in the operation of the gifts of the Holy Spirit and the gifts of
+                        Christ in the Church</p>
+
+                    <p class="wwb-title-sup"> today.</p>
+
+                    <ul class="scriptures">
+                        <li>1 Corinthians 12:4-11, 28</li>
+                        <li>Ephesians 4:7-16</li>
+                    </ul>
+                </li>
+
+
+                <li class="wwb-li">
+                    <p class="wwb-title">10. We believe in holiness of life and conduct in obedience to the command of
+                        God.</p>
+
+                    <p class="wwb-title-sup"></p>
+                    <ul class="scriptures">
+                        <li>1 Peter 1:14-16</li>
+                        <li>Hebrews 12:14 1</li>
+                        <li>Thessalonians 5:23</li>
+                        <li>1 John 2:6</li>
+                    </ul>
+                </li>
+
+
+                <li class="wwb-li">
+                    <p class="wwb-title">11. We believe that deliverance from sickness</p>
+
+                    <p class="wwb-title-sup"> by Divine Healing is provided for in the Atonement.</p>
+
+                    <ul class="scriptures">
+                        <li>Isaiah 53:4-5</li>
+                        <li>Matthew 8:16-17</li>
+                        <li>James 5:13-16</li>
+                    </ul>
+                </li>
+
+
+                <li class="wwb-li">
+                    <p class="wwb-title">12. We believe that all who have truly repented and believe in Christ as Lord
+                        and Saviour should</p>
+
+                    <p class="wwb-title-sup"> regularly participate in Breaking of Bread.</p>
+
+                    <ul class="scriptures">
+                        <li>Luke 22:14-20</li>
+                        <li>1 Corinthians 11:20-34</li>
+                    </ul>
+                </li>
+
+
+                <li class="wwb-li">
+                    <p class="wwb-title">13. We believe in the bodily resurrection of all men</p>
+
+                    <p class="wwb-title-sup"> the everlasting conscious bliss of all who truly believe in our Lord Jesus
+                        Christ and the everlasting conscious punishment of all whose names are not written in the Book
+                        of Life.</p>
+
+                    <ul class="scriptures">
+                        <li>Daniel 12:2-3</li>
+                        <li>John 5:28-29</li>
+                        <li>1 Corinthians 15:22-24</li>
+                        <li>Matthew 25:46</li>
+                        <li>2 Thessalonians 1:9</li>
+                        <li>Revelation 20:10-15</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
 
     </div>
 
